4a9968e9162f0b97c1aef9145936b6443f029316
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2020-12-14 11:26:44 -0300

    Wildcard types

---
+++ src/application/Program03.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program03 {
+
+	public static void main(String[] args) {
+	
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+		
+		List<String> myStrs = Arrays.asList("Maria", "Bob", "Alex");
+		printList(myStrs);
+		
+	}
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+	}
+}
