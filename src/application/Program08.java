467a2ea9560b0e7a2d8b52612e328f4f93491851
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2020-12-15 14:50:46 -0300

    Set

---
+++ src/application/Program08.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Program08 {
+
+	public static void main(String[] args) {
+		
+//		Set<String> set = new HashSet<>();
+//		Set<String> set = new TreeSet<>();
+		Set<String> set = new LinkedHashSet<>();
+		
+		set.add("Tv");
+		set.add("Tablet");
+		set.add("Notebook");		
+		
+//		System.out.println(set.contains("Notebook"));
+		
+//		set.remove("Tablet");
+//		set.removeIf(x -> x.length() >= 3);
+		set.removeIf(x -> x.charAt(0) == 'T');
+
+		for (String p : set) {
+			System.out.println(p);
+		}
+
+	}
+}
